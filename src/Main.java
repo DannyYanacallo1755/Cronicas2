994e663ff22619dfb0cde8dadf8527b1b120982b
DANNY\LENOVO <danny9yanacallo@gmail.com>
2023-06-19 21:54:25 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
-import Conicas1.Parabola;
 import conicas2.*;
-
 import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Scanner;
 
-/*Esta clase contiene un menú compuesto por el package del equipo 2, realizado por todos los integrantes del equipo 1
-
-* Equipo 1 - Integrantes
-* ----> Parabola:
-* ----> Hiperbole:
-*---->  Circunferencia:
-* ----> Elipse:
-
-* */
 public class Main {
     public static void main(String[] args) {
 
@@ -37,50 +26,49 @@ public class Main {
         Elipse figura8 = new Elipse(9.99, 6.66);
 
         //Creamos la pantalla del menu principal
-        System.out.println("\n\t------------------------------------Figuras 3D con Package Equipo 2----------------------------------");
+        System.out.println("\n\t*************************Figuras 3D*************************");
 
-        System.out.println("\nEl presente sistema permite al usuario el calcular diferentes operaciones de cónicas, mediante el empleo" +
-                            "de un menu y selección. \nLas conicas que se ofrecen son:");
+        System.out.println("\nMENU DE OPCIONES");
 
-        System.out.println("\n----> a. Parabola");
-        System.out.println("----> b. Hiperbole");
-        System.out.println("----> c. Circunferencia");
-        System.out.println("----> d. Elipse");
+        System.out.println("\n 1) Parabola");
+        System.out.println(" 2) Hiperbole");
+        System.out.println(" 3) Circunferencia");
+        System.out.println(" 4) Elipse");
 
-        System.out.println("\nPara continuar, seleccione la letra de la conica a usar: ");
-        String opt = entrada.next();
+        System.out.println("\nSeleccione una opcion: ");
+        int opt = entrada.nextInt();
 
 
         switch (opt) {
-            case ("a") -> {
-                System.out.println("\n-----> Ha seleccionado Parabola");
-                System.out.println("\n\t-----> Para primera instancia: ");
+            case 1 -> {
+                System.out.println("\nPARABOLA");
+                System.out.println("\n\t* Primera instancia: ");
                     figura3.Parabola_Equipo2_ImpVer();
-                System.out.println("\n\t-----> Para segunda instancia: ");
+                System.out.println("\n\t* Segunda instancia: ");
                     figura4.Parabola_Equipo2_ImpVer();
             }
-            case ("b") -> {
-                System.out.println("\n-----> Ha seleccionado Hiperbole");
-                System.out.println("\n\t-----> Para primera instancia: ");
+            case 2 -> {
+                System.out.println("\nHIPERBOLE");
+                System.out.println("\n\t* Primera instancia: ");
                     figura1.variables();
-                System.out.println("\n\t-----> Para segunda instancia: ");
+                System.out.println("\n\t* Segunda instancia: ");
                     figura2.variables();
             }
-            case ("c") -> {
-                System.out.println("\n-----> Ha seleccionado Circunferencia");
-                System.out.println("\n\t-----> Para primera instancia: ");
+            case 3 -> {
+                System.out.println("\nCIRCUNFERENCIA");
+                System.out.println("\n\t* Primera instancia: ");
                     figura5.imprimir_Datos();
-                System.out.println("\n\t-----> Para segunda instancia: ");
+                System.out.println("\n\t* Segunda instancia: ");
                     figura6.imprimir_Datos();
             }
-            case ("d") -> {
-                System.out.println("\n-----> Ha seleccionado Elipse");
-                System.out.println("\n\t-----> Para primera instancia: ");
+            case 4 -> {
+                System.out.println("\nELIPSE");
+                System.out.println("\n\t* Primera instancia: ");
                     figura7.imprimir_Datos();
-                System.out.println("\n\t-----> Para segunda instancia: ");
+                System.out.println("\n\t* Segunda instancia: ");
                     figura8.imprimir_Datos();
             }
-            default -> System.out.println("----> ERROR! Esta conica no se encuentra en el sistema!");
+            default -> System.out.println("ERROR");
         }
 
     }
